1c7f6fa62d6a0f09434c9f9f101858b877fcf9a7
akhil-2709 <aanthireddy@umass.edu>
2023-06-27 00:10:18 -0400

    Connected backend to Database

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -24,4 +25,11 @@ app.use((error, req, res, next) => {
     res.json({message: error.message || 'An unknown error occurred!'})
 })
 
-app.listen(5001);
+mongoose
+    .connect('mongodb+srv://aredd127:Jobless123@cluster0.d6dtp92.mongodb.net/places?retryWrites=true&w=majority')
+    .then(() => {
+        app.listen(5001);
+    })
+    .catch(err => {
+        console.log(err)
+    });
