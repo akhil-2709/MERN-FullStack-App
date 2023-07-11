c4a1ec891ba720991416fa7bb4099815dd9bbd14
Akhil Reddy <100547384+akhil-2709@users.noreply.github.com>
2023-07-11 17:42:25 -0400

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-    .connect('mongodb+srv://aredd127:Jobless123@cluster0.d6dtp92.mongodb.net/mern?retryWrites=true&w=majority')
+    .connect('mongodb+srv://<user>:<password>@cluster0.d6dtp92.mongodb.net/mern?retryWrites=true&w=majority')
     .then(() => {
         app.listen(5001);
     })
